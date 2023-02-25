7a2f13873646980ef555f2d4542a18aa67f7d0d6
TILIKU <85792875+TILIKU@users.noreply.github.com>
2023-02-25 17:48:58 +0400

    i have no idea where this is being commited tbh

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from "./styles"
+
+const Header = () => {
+
+    const classes = useStyles()
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5"  className={classes.title}>
+                    travel guy
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6"  className={classes.title}>
+                        explore deez nuts
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div    className={classes.seachIcon}>
+                                <seachIcon />
+                            </div>
+                            <InputBase placeholder="search..." classes={{ root:classes.inputRoot, input:classes.inputInput }}/>
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
